85ebcaa8014dd71d86273647d093dba85973b98a
meregh <meregh.istvan@gmail.com>
2026-02-25 11:59:39 +0100

    5 - Új termék felvitele (POST)

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -50,6 +50,28 @@ router.put('/:azonosito', function (keres, valasz) {
 //termék létrehozása
 //TODO
 
+router.post('/', function (keres, valasz) {
+    
+    const kategoriaId = keres.body.kategoriaId;
+    const termekNev = keres.body.termekNev;
+    const ar = keres.body.ar;
+    const darabSzam = keres.body.darabSzam;
+
+
+    const sql = "INSERT INTO `termekek`(`kategoria_id`, `nev`, `egysegar`, `keszlet_db`) VALUES(?,?,?,?)";
+    const variables = [kategoriaId, termekNev, ar, darabSzam];
+
+
+    adatbazis.query(sql, variables, function (hiba, eredmeny) {
+        if (hiba) {
+            return valasz.status(400).json({
+                "valasz": "Az ár és a darabszám nem lehet negatív érték!"
+            });
+        }
+        valasz.status(201).json({ "uzenet": "A termék rögzítésre került!" });
+    })
+})
+
 
 //termék törlése
 //TODO
