c556c4f77983e87c3804fa4a485be783060f40cb
Szoke Mark <Lupika666@gmail.com>
2026-02-25 11:56:50 +0100

    #7 Kategória módosítása (PUT) route elkészítve

--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -14,7 +14,31 @@ router.get("/", function (keres, valasz) {
 })
 
 //kategória módosítása
-//TODO
+router.put("/:id", function (keres, valasz) {
+    const kategoria_id = keres.params.id
+    const kategoriaNev = keres.body.kategoriaNev
+
+    if (!kategoriaNev) {
+        return valasz.status(400).json({
+            "valasz": "Nem adta meg a kategórianevet!"
+        })
+    }
+
+    const sql = "UPDATE kategoriak SET nev = ? WHERE id = ?"
+
+    adatbazis.query(sql, [kategoriaNev, kategoria_id], function (hiba, eredmeny) {
+        if (eredmeny.affectedRows < 1) {
+            return valasz.status(404).json({
+                "valasz": "Nincs ilyen kategória a rendszerben!"
+            })
+        }
+        valasz.status(200).json({
+            "uzenet": "Sikeres frissítés",
+            "id": valasz.body.id,
+            "kategoriaNev": valasz.body.nev
+        })
+    })
+})
 
 //kategória létrehozása
 router.post('/', function (keres, valasz) {
