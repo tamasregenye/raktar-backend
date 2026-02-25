b3cbf4ca62e43dd585431957239eda3d06863ee5
petomelinda <lyandaster@gmail.com>
2026-02-25 08:21:05 +0100

    órai anyag

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const adatbazis = require('../adatbazis');
+
+router.get("/", function (keres, valasz) {
+    const sql = "SELECT * FROM kategoriak";
+    adatbazis.query(sql, function (hiba, eredmeny) {
+        if (hiba) {
+            return valasz.status(500).json({ "valasz": hiba.message });
+        }
+        valasz.status(200).json(eredmeny);
+    })
+})
+
+router.post('/', function (keres, valasz) {
+    const kategoriaNev = keres.body.kategoriaNev;
+    const sql = "INSERT INTO `kategoriak`(`nev`) VALUES (?)";
+
+    if (!kategoriaNev) {
+        return valasz.status(400).json({
+            "valasz": "Nem adta meg a kategórianevet!"
+        })
+    }
+
+    adatbazis.query(sql, [kategoriaNev], function (hiba, eredmeny) {
+        if (hiba) {
+            return valasz.status(500).json({
+                "valasz": hiba.message
+            });
+        }
+        valasz.status(201).json({ "uzenet": "A feladat rögzítésre került!" });
+    })
+})
+
+
+module.exports = router; 
