746dcf072fcb196a72f3c353bd333d3f09dccaa2
Pelczhoffer Éva <ec25-pelczhoffer@ipari.vein.hu>
2026-02-26 10:34:13 +0100

    2026.02.26

--- routes/movementRoutes.js
+++ routes/movementRoutes.js
@@ -2,6 +2,61 @@ const express = require('express');
 const router = express.Router();
 const adatbazis = require('../adatbazis');
 
+/**
+ * @swagger
+ * tags:
+ *    name: Mozgások 
+ *    description: Raktármozgások kezelése
+ */
+
+
+//mozgások módosítása
+
+/**
+ * @swagger
+ * /api/mozgasok/{mozgasId}:
+ *   put:
+ *     summary: "Raktármozgás módosítása"
+ *     description: "Ez a végpont teszi lehetővé egy meglévő raktármozgás adatainak módosítását."
+ *     tags: ["Mozgások"]
+ *     parameters: 
+ *       - in: path
+ *         name: mozgasId
+ *         required: true
+ *         type: number
+ *     requestBody:
+ *       reqired: true
+ *       content: 
+ *         application/json:
+ *           schema:
+ *             properties:
+ *               termekId:
+ *                 type: number
+ *                 description: "A mozgáshoz kapcsolodó termék azonosítója"
+ *               partnerId:
+ *                 type: number
+ *                 description: "A mozgáshoz kapcsolodó partner azonosítója"
+ *               mennyiseg:
+ *                 type: number
+ *                 description: "A mozgás mennyisége"
+ *               datum:
+ *                 type: string
+ *                 format: date-time
+ *                 description: "A mozgás dátuma és időpontja"
+ *     responses: 
+ *        200:
+ *          description: "Sikeres módosítás"
+ *        400:
+ *          description: "Nem adta meg a szükséges adatot"
+ *        404:
+ *          description: "Nincs ilyen mozgás"
+ *        500:
+ *          description: "Hiba a szerveren"
+ *      
+ * 
+ */    
+
+
 
 router.put("/:mozgasId", function (keres, valasz) {
     const mozgasId = keres.params.mozgasId;
