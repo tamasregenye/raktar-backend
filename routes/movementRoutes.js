d20c5b4111dc3f70dbd97fa8e3827ce397a42c32
dioszegi <diobence@gmail.com>
2026-02-26 10:34:11 +0100

    feb26. óra swagger

--- routes/movementRoutes.js
+++ routes/movementRoutes.js
@@ -2,6 +2,56 @@ const express = require('express');
 const router = express.Router();
 const adatbazis = require('../adatbazis');
 
+/**
+ * @swagger
+ * tags:
+ *   name: Mozgások
+ *   description: Raktár mozgások kezelése
+ */
+
+//mozgások módosítása
+
+/**
+ * @swagger
+ * /api/mozgasok/{mozgasId}:
+ *   put:
+ *     summary: "Raktár mozgás módosítása"
+ *     description: "Ez a végpont teszi lehetővé egy meglévő raktár mozgás adatainak módosítását az adatbázisban."
+ *     tags: ["Mozgások"]
+ *     parameters:
+ *       - in: path
+ *         name: mozgasId
+ *         required: true
+ *         type: number
+ *     requestBody:
+ *       required: true
+ *       content: 
+ *         application/json:
+ *           schema: 
+ *             properties:
+ *               termekId:
+ *                 type: number
+ *                 description: "A mozgáshoz kapcsolódó termék azonosítója"
+ *               partnerId:
+ *                 type: number
+ *                 description: "A mozgáshoz kapcsolódó partner azonosítója"
+ *               mennyiseg:
+ *                 type: number
+ *                 description: "A mozgás mennyisége - negatív vagy pozitív érték is lehet"
+ *               datum:
+ *                 type: string
+ *                 format: date-time
+ *                 description: "A mozgás dátuma és időpontja"
+ *     responses:
+ *       200: 
+ *         description: "Sikeres módosítás!"
+ *       400: 
+ *         description: "Hibás kérés, nem adta meg a szükséges adatokat!"
+ *       404: 
+ *         description: "Hibás kérés, a megadott azonosítóval nem létezik rekord!"
+ *       500: 
+ *         description: "Hiba történt a szerveren, nem sikerült módosítani a mozgást!"
+ */
 router.put("/:mozgasId", function (keres, valasz) {
     const mozgasId = keres.params.mozgasId;
     const termekId = keres.body.termekId;
