0f23011ca84c2ae39d4d6ef8f5fbc57546d3199a
Szoke Mark <Lupika666@gmail.com>
2026-02-26 10:34:33 +0100

    Órai munka

--- routes/movementRoutes.js
+++ routes/movementRoutes.js
@@ -2,11 +2,61 @@ const express = require('express');
 const adatbazis = require("../adatbazis")
 const router = express.Router();
 
+
+/**
+ * @swagger
+ * tags:
+ *  name: Mozgások
+ *  descriptiopn: Raktár mozgások kezelése
+ */
+
+
+/**
+ * @swagger
+ * /api/mozgasok/{mozgasId}:
+ *  put:
+ *      summary: "Raktár mozgás módosítása"
+ *      description: "Ez a végpont teszi lehetővé egy meglévő raktár mozgás adatainak módosítását"
+ *      tags: ["Mozgások"]
+ *      parameters:
+ *          - in: path
+ *            name: mozgasId
+ *            required: true
+ *            type: number
+ *      requestBody:
+ *          required: true
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      properties:
+ *                          termekId:
+ *                              type: number
+ *                              description: "A mozgáshoz kapcsolódó termék azonosítója"
+ *                          partnerId:
+ *                              type: number
+ *                              description: "A mozgáshoz kapcsolódó partner azonosítója"
+ *                          mennyiseg:
+ *                              type: number
+ *                              description: "A mozgáshoz mennyisége"
+ *                          datum:
+ *                              type: string
+ *                              format: date-time
+ *                              description: "A mozgás dátuma"
+ *      responses:
+ *          200:
+ *              description: "Sikeres módosítás!"
+ *          400:
+ *              description: "Nem adata meg a szükséges adatokat!"
+ *          404:
+ *              description: "Nincs ilyen mozgás!"
+ *          500:
+ *              description: "Szerver hiba!"      
+ */
 router.put("/:mozgasId", function (keres, valasz) {
     const mozgasId = keres.params.mozgasId
     const { termekId, partnerId, mennyiseg, datum } = keres.body
     if (!mozgasId || !termekId || !partnerId || !mennyiseg || !datum) {
-        return valasz.status(500).json({
+        return valasz.status(400).json({
             "valasz": "Nem adata meg a szükséges adatokat!"
         })
     }
