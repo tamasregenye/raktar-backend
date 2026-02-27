28f1de36d0b28a0a4be84dcc81add4cf7049388b
farkas-geza <ec25-farkas@ipari.vein.hu>
2026-02-27 12:05:36 +0100

    Órai munka

---
+++ models/categoryModel.js
@@ -0,0 +1,15 @@
+const adatbazis = require('../adatbazis');
+
+const categoryModel = {
+    selectAllCategories: (callback) => {
+         const sql = "SELECT * FROM kategoriak";
+        adatbazis.query(sql, callback);
+    },
+
+    insertCategory: (kategoriaNev, callback) => {
+        const sql = "INSERT INTO `kategoriak`(`nev`) VALUES (?)";
+        adatbazis.query(sql, [kategoriaNev], callback);
+    }
+}
+
+module.exports = categoryModel;
