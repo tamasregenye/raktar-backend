7d96c9f14682d11f430c92e81b2e5bc2cca4768c
peto_melinda <70775647+pm-007417@users.noreply.github.com>
2026-03-04 12:32:29 +0100

    ora 03.04.

---
+++ models/productModel.js
@@ -0,0 +1,16 @@
+const { useCallback } = require('react');
+const adatbazis = require('../adatbazis');
+
+const productModel = {
+    selectAllProducts: (callback) => {
+        const sql = "SELECT id, kategoria_id AS 'kategoriaAzonosito', nev AS 'termekNev', egysegar AS 'ar', keszlet_db AS 'keszleten' FROM `termekek`";
+        adatbazis.query(sql, callback);
+    },
+
+    updateProduct: (azonosito, termek, callback) => {
+        const sql = "UPDATE `termekek` SET `kategoria_id`=?,`nev`=?,`egysegar`=?,`keszlet_db`=? WHERE `id`=?";
+        adatbazis.query(sql, [termek.kategoriaId, termek.termekNev, termek.ar, termek.darabSzam, azonosito], callback)
+    }
+}
+
+module.exports = productModel;
