3fffc74c87774cb24254800fe247ff243538a184
Tamas Regenye <tomiregenye@gmail.com>
2026-03-04 09:44:58 +0100

    termékek - MVC

---
+++ models/productModel.js
@@ -0,0 +1,15 @@
+const adatbazis = require("../adatbazis");
+
+const productModel = {
+    selectProducts: (callback) => {
+        const sql = "SELECT id, kategoria_id AS 'kategoriaAzonosito', nev AS 'termekNev', egysegar AS 'ar', keszlet_db AS 'keszleten' FROM `termekek`";
+        adatbazis.query(sql, callback);
+    },
+
+    updateProduct: (azonosito, termek, callback) =>{
+        const sql = "UPDATE `termekek` SET `kategoria_id`=?,`nev`=?,`egysegar`=?,`keszlet_db`=? WHERE `id`=?";
+        adatbazis.query(sql, [termek.kategoriaId, termek.termekNev, termek.ar, termek.darabSzam, azonosito], callback)
+    }
+}
+
+module.exports = productModel
