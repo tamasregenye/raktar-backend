aa59cd415e3023a08b6010bb6ab06b8deb2b9991
Vera9811 <hegv98@gmail.com>
2026-03-05 12:47:07 +0100

    03055

---
+++ dumps/felhasznalok.sql
@@ -0,0 +1,13 @@
+/* felhasználók tábla létrehpzása */
+/*TODO futtatni a teszt és a rendes adatbázison is */
+
+CREATE TABLE felhasznalok (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    jelszo VARCHAR(100) NOT NULL,
+    szerepkor ENUM("user","admin") DEFAULT "user",
+    nev VARCHAR(100) NOT NULL,
+    aktiv TINYINT(1) DEFAULT(1),
+    letrehozva TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    legutobbi_bejelentkezes TIMESTAMP NULL DEFAULT NULL,
+    )
---
+++ utils/validationHelper.js
@@ -0,0 +1,19 @@
+const {validationResult} = require ("express-validator")
+
+const validateRequest = (keres, valasz, next) => {
+        const errors = validationResult(keres);
+
+        //ha van validációs hiba
+        if (!errors.isEmpty()) {
+            return valasz.status(400).json({
+                "valasz": "Validációs hiba!",
+                "hibak": errors.array().map(err => (
+                    { "mezo": err.path, "uzenet": err.msg }
+                ))
+            })
+        }
+        //ha nincs validciós hiba, tovább engedjük
+        next();
+    }
+
+module.exports = {validateRequest}
