2b754d1788c69702219ef580c1ecb174070ed0a8
Tamas Regenye <tomiregenye@gmail.com>
2026-03-06 10:48:10 +0100

    regisztrációnál megadott adatok validálása

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const { methodNotAllowed } = require('../utils/errors');
+const { authRegisterUserValidator } = require('../validators/authValidator');
 const router = express.Router();
 
 //végpontok definiálása
 
 //TODO validálás
-router.post('/regisztracio', authController.registerUser)
+router.post('/regisztracio', authRegisterUserValidator, authController.registerUser)
 
 //hibás HTTP metódus megadása esetén 405 státusz küldése
 router.all('/regisztacio', methodNotAllowed)
