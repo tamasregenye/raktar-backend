608973a7f8c982f2e63f68c25065de1e11366824
Csabesz <ec25-palko@ipari.vein.hu>
2026-03-06 08:11:25 +0100

    help

--- tests/productPut.test.js
+++ tests/productPut.test.js
@@ -9,23 +9,6 @@ app.use(express.json());
 //tesztelni kívánt végpont
 app.put("/api/termekek/:azonosito", productPutValidator, productController.putProduct);
 
-<<<<<<< HEAD
-describe('PUT /api/termekek/:azonosito tesztelése', () => {
-    
-    //1. teszteset
-    it('400-as hibát kell dobnia, ha az azonosító nem szám', async () => {
-        
-        //kérés küldése a szervernek
-        //szerver válasza eltárolva egy 'valasz' változóban
-        const valasz = await request(app)
-        .put('/api/termekek/ipari')
-        .send({
-            "kategoriaId": 1,
-            "termekNev": "Kalapács",
-            "ar": 3000,
-            "darabSzam": 10
-        });
-=======
 //csoport a tesztekhez
 describe('PUT /api/termekek/:azonosito tesztelése', () => {
 
@@ -42,60 +25,11 @@ describe('PUT /api/termekek/:azonosito tesztelése', () => {
                 "ar": 3000,
                 "darabSzam": 10
             });
->>>>>>> origin/main
 
         expect(valasz.status).toBe(400); // if(valasz.status === 400)
         expect(valasz.body.hibak.find(err => err.mezo === 'azonosito')).toBeTruthy();
     });
 
-<<<<<<< HEAD
-    //4. teszteset OK 200
-    it('200-as hibát kell dobnia és ténylegesen módosítást kell végrehajtania az adatbázisban', async () => {
-        
-        //kérés küldése a szervernek
-        //szerver válasza eltárolva egy 'valasz' változóban
-        const valasz = await request(app)
-        .put('/api/termekek/6')
-        .send({
-            "kategoriaId": 2,
-            "termekNev": "Széf tele 24 karátos arany rögökkel",
-            "ar": 59000000,
-            "darabSzam": 2
-        });
-
-        expect(valasz.status).toBe(200); // if(valasz.status === 200)
-        //expect(valasz.body.hibak).toBeUndefined();
-    });
-
-    //2. teszt
-    //a kategória id nem nagyobb nullánál akkor 400-as hibakód.
-    it('400-as hibát kell dobnia ha a kategória Id nem nagyobb nullánál', async () => {
-     
-    const valasz = await request(app)
-        .put('/api/termekek/1')
-        .send({
-            "kategoriaId": 0,
-            "termekNev": "Kalapács",
-            "ar": 3000,
-            "darabSzam": 10
-        });
-
-        expect(valasz.status).toBe(400); // if(valasz.status === 400)
-        expect(valasz.body.hibak.find(err => err.mezo === 'kategoriaId')).toBeTruthy();
-    });
-
-    //3. teszteset ha a terméknév nem szöveg vagy nincs megadva
-    it('400-as hibát kell dobnia ha a terméknév nem szöveg vagy nincs megadva', async () => {
-     
-    const valasz = await request(app)
-        .put('/api/termekek/1')
-        .send({
-            "kategoriaId": 1,
-            "termekNev": 2,
-            "ar": 3000,
-            "darabSzam": 10
-        });
-=======
     //2. teszteset
     it('400-as hibát kell dobnia, ha a terméknév szám', async () => {
 
@@ -109,60 +43,11 @@ describe('PUT /api/termekek/:azonosito tesztelése', () => {
                 "ar": 3000,
                 "darabSzam": 10
             });
->>>>>>> origin/main
 
         expect(valasz.status).toBe(400); // if(valasz.status === 400)
         expect(valasz.body.hibak.find(err => err.mezo === 'termekNev')).toBeTruthy();
     });
 
-<<<<<<< HEAD
-    it('400-as hibát kell dobnia ha a terméknév nincs megadva', async () => {
-     
-    const valasz = await request(app)
-        .put('/api/termekek/1')
-        .send({
-            "kategoriaId": 1,
-            "ar": 3000,
-            "darabSzam": 10
-        });
-
-        expect(valasz.status).toBe(400); // if(valasz.status === 400)
-        expect(valasz.body.hibak.find(err => err.mezo === 'termekNev')).toBeTruthy();
-    });
-    
-    //5. teszteset: az ár nem pozitív szám
-
-    it('400-as hibát kell dobnia ha az ár negatív szám', async () => {
-     
-    const valasz = await request(app)
-        .put('/api/termekek/1')
-        .send({
-            "kategoriaId": 1,
-            "termekNev": 2,
-            "ar": -3000,
-            "darabSzam": 10
-        });
-
-        expect(valasz.status).toBe(400); // if(valasz.status === 400)
-        expect(valasz.body.hibak.find(err => err.mezo === 'ar')).toBeTruthy();
-    });
-    
-    //6. teszteset: ha a darabszám negatív
-    it('400-as hibát kell dobnia ha az ár negatív szám', async () => {
-     
-    const valasz = await request(app)
-        .put('/api/termekek/1')
-        .send({
-            "kategoriaId": 1,
-            "termekNev": 2,
-            "ar": 3000,
-            "darabSzam": -10
-        });
-
-        expect(valasz.status).toBe(400); // if(valasz.status === 400)
-        expect(valasz.body.hibak.find(err => err.mezo === 'darabSzam')).toBeTruthy();
-    });
-=======
     //4. teszteset OK
     it('200-as státuszt kell dobnia és tényleges módosítást kell végrehajtania az adatbázisban', async () => {
         const valasz = await request(app)
@@ -176,7 +61,6 @@ describe('PUT /api/termekek/:azonosito tesztelése', () => {
         expect(valasz.status).toBe(200);
     });
 
->>>>>>> origin/main
 });
 
 
